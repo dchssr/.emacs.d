6c1440a3f7b66ef7899b0ffca95fab70bd523324
Dave Chesser <me@dchesser.website>
2020-08-19 23:59:19 -0400

    package: use-package

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
 (require 'package)
 (add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/") t)
 (package-initialize)
+
+;; The rest of this is going to be use-package'd.
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t)
