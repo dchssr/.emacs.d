0535178e217b7a2a0fa4bf7c8620734dad7a650f
Dave Chesser <me@dchesser.website>
2020-08-22 03:23:47 -0400

    compat: initialize packages when version is before Emacs 27
    I'm not guaranteed that any distro I use will have any Emacs later
    than 26 in their package repositories.
    May as well accomodate this possibility until then.

--- init.el
+++ init.el
@@ -1,8 +1,11 @@
 (require 'package)
 (add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/") t)
-(package-initialize)
 
-;; The rest of this is going to be use-package'd.
+;; Starting with Emacs 27, packages are now activated before loading
+;; this init script.
+(when (< emacs-major-version 27)
+  (package-initialize))
+
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
