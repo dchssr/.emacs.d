abeefffb1e755a35b300e3a7e3d5f491089b4f38
Dave Chesser <me@dchesser.website>
2021-04-11 12:17:34 -0400

    fix: refresh package descriptions before installing use-package
    use-package won't be available if the package listing isn't refreshed.
    This at leat makes the configuration ready to go after cloning.

--- init.el
+++ init.el
@@ -7,7 +7,10 @@
   (package-initialize))
 
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
+
+(require 'use-package)
 
 (setq use-package-always-ensure t)
 
