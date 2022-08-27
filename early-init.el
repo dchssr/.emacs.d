9be207e034663f0935262c036fe0387e5ce0cca4
Dave Chesser <me@dchesser.website>
2022-08-27 13:54:15 -0400

    fix: untabify code

--- early-init.el
+++ early-init.el
@@ -3,10 +3,10 @@
 
 (let ((chosen-font-face
        (concat (D-available-preferred-font)
-	        "-" (number-to-string D-preferred-font-size))))
+                "-" (number-to-string D-preferred-font-size))))
   (setq initial-frame-alist
-	`((width . 80)		; 80-character lines are common in style guides
-	  (tool-bar-lines . 0)
-	  (font . ,chosen-font-face))
-	inhibit-startup-screen t
-	default-frame-alist initial-frame-alist))
+        `((width . 80) ; 80-character lines are common in style guides
+          (tool-bar-lines . 0)
+          (font . ,chosen-font-face))
+        inhibit-startup-screen t
+        default-frame-alist initial-frame-alist))
